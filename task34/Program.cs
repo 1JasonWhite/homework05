7edca2fe677fdee4fc7357f60f908789ac6aaca6
Pavel <krava2007_87@mail.ru>
2023-03-15 19:52:09 +1000

    add tasks 34, 36, 38

---
+++ task34/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 34: 
+
+// 1. Задайте массив заполненный случайными положительными трёхзначными числами. 
+// 2. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max) //Задаем массив
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr) //Выводим массив на экран
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int EvenNumbers(int[] arr)  //четные числа
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+int[] array = CreateArrayRndInt(5, 99, 999);
+PrintArray(array);
+int evenNumbers = EvenNumbers(array);
+Console.WriteLine($"Количество чётных чисел = {evenNumbers}");
---
+++ task36/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 36: 
+
+// 1. Задайте одномерный массив, заполненный случайными числами.
+// 2. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max) //Задаем массив
+{
+    int[] arr = new int[size];
+    Random rnd = new Random(); 
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr) //Выводим массив на экран
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int OddSumNumbers(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 != 0)
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+int[] array = CreateArrayRndInt(6, 0, 10);
+PrintArray(array);
+int oddSumNumbers = OddSumNumbers(array);
+Console.WriteLine($"Сумма нечетных чисел = {oddSumNumbers}");
